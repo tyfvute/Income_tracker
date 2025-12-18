d2ee2e2301c37d159557552ed1c2a2c0b1300e55
tyfvute <kurlekstraw@mail.ru>
2025-12-18 20:19:10 +0400

    Update main.py
    Добавил удаление

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from database import get_db_connection, init_db
 import sqlite3
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = 'admin'
@@ -15,34 +16,57 @@ def index():
     conn = get_db_connection()
     cursor = conn.cursor()
     user_id = session['user_id']
-    username = session['username']
 
     if request.method == 'POST':
-        amount = request.form['amount']
+        amount = int(float(request.form['amount']))
         category = request.form['category']
         rec_type = request.form['type']
 
+        current_time = datetime.now().strftime("%d.%m.%Y %H:%M")
+
         if amount and category and rec_type:
+
             cursor.execute(
-                "INSERT INTO records (user_id, amount, category, type) VALUES (?, ?, ?, ?)",
-                (user_id, amount, category, rec_type)
+                "INSERT INTO records (user_id, amount, category, type, date) VALUES (?, ?, ?, ?, ?)",
+                (user_id, amount, category, rec_type, current_time)
             )
             conn.commit()
             return redirect(url_for('index'))
 
-    cursor.execute("SELECT * FROM records WHERE user_id = ? ORDER BY date DESC", (user_id,))
+    cursor.execute("SELECT * FROM records WHERE user_id = ? ORDER BY id DESC", (user_id,))
     records = cursor.fetchall()
 
     total_balance = 0
     for r in records:
         if r['type'] == 'income':
-            total_balance += r['amount']
+            total_balance += int(r['amount'])
         else:
-            total_balance -= r['amount']
+            total_balance -= int(r['amount'])
 
+    cursor.execute(
+        "SELECT category, SUM(amount) as total FROM records WHERE user_id = ? AND type = 'expense' GROUP BY category",
+        (user_id,))
+    expense_stats = cursor.fetchall()
     conn.close()
 
-    return render_template('index.html', username=username, records=records, balance=total_balance)
+    return render_template('index.html', username=session['username'], records=records, balance=total_balance,
+                           stats=expense_stats)
+
+@app.route('/delete/<int:record_id>')
+def delete_record(record_id):
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM records WHERE id = ? AND user_id = ?", (record_id, session['user_id']))
+
+    conn.commit()
+    conn.close()
+
+    flash('Запись удалена')
+    return redirect(url_for('index'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
